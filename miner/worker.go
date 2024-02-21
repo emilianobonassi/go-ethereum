bc0c7755d960fdf5c8e5a3bb4a2265da4504f2d6
Emiliano Bonassi <emiliano@conduit.xyz>
2024-02-21 17:43:32 +0100

    fix(miner): fix npe cancun enabled at genesis

--- miner/worker.go
+++ miner/worker.go
@@ -987,7 +987,7 @@ func (w *worker) prepareWork(genParams *generateParams) (*environment, error) {
 	// Apply EIP-4844, EIP-4788.
 	if w.chainConfig.IsCancun(header.Number, header.Time) {
 		var excessBlobGas uint64
-		if w.chainConfig.IsCancun(parent.Number, parent.Time) {
+		if w.chainConfig.IsCancun(parent.Number, parent.Time) && parent.Number.Cmp(big.NewInt(0)) != 0 {
 			excessBlobGas = eip4844.CalcExcessBlobGas(*parent.ExcessBlobGas, *parent.BlobGasUsed)
 		} else {
 			// For the first post-fork block, both parent.data_gas_used and parent.excess_data_gas are evaluated as 0
